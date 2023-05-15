b44ec7104002264f1b8241608edb2ece8705e49d
Michelle B <axew13mail@gmail.com>
2023-05-15 01:19:17 +0100

    "It's just an augmented reality demo, you don't need to go so hard on the dialogue system"

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,7 +1,8 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
+using System.IO;
 
 public class DialogueManager : MonoBehaviour
 {
@@ -10,29 +11,75 @@ public class DialogueManager : MonoBehaviour
 
     private ArrayList cache = new ArrayList();
 
+    // Store an individual line. Pretty annoying to add multiple lines with
     public void cacheDialogue (string text)
     {
         cache.Add(text);
     }
 
+    // Search for lines from txt files in a "Text" folder and store them
+    public void cacheDialogueFromFile(string textFile)
+    {
+        try
+        {
+            using (StreamReader file = new StreamReader("Text/" + textFile + ".txt"))
+            {
+                int counter = 0;
+                string ln;
+
+                while ((ln = file.ReadLine()) != null)
+                {
+                    cache.Add(ln);
+                    counter++;
+                }
+                file.Close();
+            }
+        }
+            catch (Exception ex)
+        {
+            // Just in case things don't work in a build
+            cache.Add("Error: " + ex.Message);
+        }
+    }
+
+    // Run special code based on the content of lines
+    public void runEvent (string percentEvent) {
+
+    }
+
+    // Display stored lines on-screen
     public void nextPrompt ()
     {
         if ( cache.Count == 0)
         {
+            // If cache of lines is empty, close the dialogue box
             closePrompt(false);
         }
             else
         {
-            if (!prompt.activeSelf)
-            {
-                prompt.SetActive(true);
-            }
-
-            textMesh.SetText((string) cache[0]);
+            string ln = (string) cache[0];
             cache.RemoveAt(0);
+
+            if (ln.StartsWith("%"))
+            {
+                // If next line is an event to run, run it and skip to the next line
+                runEvent(ln);
+                nextPrompt();
+            }
+                else
+            {
+                if (!prompt.activeSelf)
+                {
+                    // If the dialogue box is closed, open it
+                    prompt.SetActive(true);
+                }
+
+                textMesh.SetText(ln);
+            }
         }
     }
 
+    // Close the dialogue box. Not necessary to run unless you want to interrupt stored lines. The current line will be erased
     public void closePrompt(bool eraseCache)
     {
         if (eraseCache)
